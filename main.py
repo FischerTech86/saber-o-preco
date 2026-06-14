35e0bf26fa26f74c807c6f8412493d36d29b6139
FischerTech86 <peixinho1533@gmail.com>
2026-06-13 23:18:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def index():
     if request.method == "POST":
         termo = request.form.get("produto")
         if termo:
-            # Substitui espaços por %20 para URLs funcionarem
+            # Substitui espacos por %20 para a URL funcionar
             t = termo.replace(" ", "%20")
             lojas = [
                 {"nome": "Mercado Livre", "link": f"https://lista.mercadolivre.com.br/{t}"},
