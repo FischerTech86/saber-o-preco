bc14c81c0804fa726064e0da42ce89b2475e6245
FischerTech86 <peixinho1533@gmail.com>
2026-06-14 11:33:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ def index():
     if request.method == "POST":
         termo = request.form.get("produto")
         if termo:
-            # Substitui espacos por %20 para a URL ser valida
+            # Substitui espaços por %20 para a URL funcionar em qualquer site
             t = termo.replace(" ", "%20")
             lojas = [
                 {"nome": "Mercado Livre", "link": f"https://lista.mercadolivre.com.br/{t}"},
                 {"nome": "Amazon", "link": f"https://www.amazon.com.br/s?k={t}"},
                 {"nome": "Americanas", "link": f"https://www.americanas.com.br/busca/{t}"},
-                {"nome": "Carrefour", "link": f"https://www.carrefour.com.br/busca/{t}"},
+                {"nome": "Carrefour", "link": f"https://www.carrefour.com.br/busca/?q={t}"},
                 {"nome": "Magalu", "link": f"https://www.magazineluiza.com.br/busca/{t}/"},
                 {"nome": "Shopee", "link": f"https://shopee.com.br/search?keyword={t}"},
                 {"nome": "Kabum", "link": f"https://www.kabum.com.br/busca/{t}"},
                 {"nome": "Casas Bahia", "link": f"https://www.casasbahia.com.br/busca/?q={t}"},
-                {"nome": "Extra", "link": f"https://www.clubeextra.com.br/busca/{t}"},
-                {"nome": "Assai", "link": f"https://www.assai.com.br/busca/{t}"},
+                {"nome": "Extra", "link": f"https://www.clubeextra.com.br/busca/?q={t}"},
+                {"nome": "Assai", "link": f"https://www.assai.com.br/busca?q={t}"},
                 {"nome": "Tenda Atacadista", "link": f"https://www.tendaatacado.com.br/busca?q={t}"},
                 {"nome": "Oxxo", "link": f"https://www.mercadooxxo.com.br/busca?q={t}"},
-                {"nome": "Coop", "link": f"https://www.portalcoop.com.br/busca?q={t}"},
+                {"nome": "Coop", "link": f"https://www.portalcoop.com.br/busca/?q={t}"},
                 {"nome": "Swift", "link": f"https://www.swift.com.br/busca?q={t}"}
             ]
     return render_template("index.html", lojas=lojas, termo=termo)
