eee461421cefce702dddf7fbea327105343b8105
FischerTech86 <peixinho1533@gmail.com>
2026-06-14 12:05:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ def index():
         if termo:
             t = termo.replace(" ", "%20")
             lojas = [
-                # Foco em mercados e grandes varejistas:
+                # Foco em mercados que permitem busca direta:
                 {"nome": "Carrefour", "link": f"https://www.carrefour.com.br/busca/?q={t}"},
                 {"nome": "Sonda", "link": f"https://www.sondadelivery.com.br/delivery/busca/{t}"},
                 {"nome": "Tenda Atacadista", "link": f"https://www.tendaatacado.com.br/busca?q={t}"},
-                # Mantendo as grandes lojas de referência:
+                {"nome": "Dia", "link": f"https://www.dia.com.br/busca?q={t}"},
+                {"nome": "Extra", "link": f"https://www.clubeextra.com.br/busca/?q={t}"},
+                {"nome": "Roldão", "link": f"https://www.roldao.com.br/busca?q={t}"},
+                # Grandes varejistas:
                 {"nome": "Mercado Livre", "link": f"https://lista.mercadolivre.com.br/{t}"},
                 {"nome": "Amazon", "link": f"https://www.amazon.com.br/s?k={t}"}
             ]
