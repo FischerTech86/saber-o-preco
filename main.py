3c12a099c3de7bcbd8ea43211fd834905d14ebdc
FischerTech86 <peixinho1533@gmail.com>
2026-06-19 11:16:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    query = None
     if request.method == 'POST':
-        query = request.form.get('produto', '').strip()
-    return render_template('index.html', query=query)
+        produto = request.form.get('produto', '').strip()
+        if produto:
+            # Redireciona para o Google Shopping diretamente
+            return redirect(f"https://www.google.com/search?q={produto}&tbm=shop")
+    
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
