45a73f4435db4ea1e713790c116397717453464d
FischerTech86 <peixinho1533@gmail.com>
2026-06-19 11:00:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
-# O Flask já procura a pasta 'static' automaticamente se ela estiver na raiz
 app = Flask(__name__)
-
-DB = {
-    "carro": "https://www.webmotors.com.br",
-    "relogio": "https://www.vivara.com.br",
-    "mercado": "https://www.carrefour.com.br"
-}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    query = None
     if request.method == 'POST':
-        produto = request.form.get('produto', '').lower()
-        if produto in DB:
-            return redirect(DB[produto])
-        return render_template('index.html', msg="Produto não encontrado.")
-    return render_template('index.html')
+        query = request.form.get('produto', '').strip()
+    
+    return render_template('index.html', query=query)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
