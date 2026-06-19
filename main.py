fed49c72c0fc89e1f350e7dde5025bab42c743d2
FischerTech86 <peixinho1533@gmail.com>
2026-06-19 10:20:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Dados básicos
+# Dados
 LISTA_MERCADOS = [{"nome": "Carrefour", "link": "https://www.carrefour.com.br"}, {"nome": "Tenda", "link": "https://www.tendaatacado.com.br"}]
 LISTA_LOJAS = [{"nome": "Mercado Livre", "link": "https://www.mercadolivre.com.br"}, {"nome": "Amazon", "link": "https://www.amazon.com.br"}]
 LISTA_CARROS = [{"nome": "Webmotors", "link": "https://www.webmotors.com.br"}, {"nome": "OLX Carros", "link": "https://www.olx.com.br/autos"}]
@@ -11,15 +11,23 @@ LISTA_RELOGIOS = [{"nome": "Vivara", "link": "https://www.vivara.com.br"}, {"nom
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    resposta_ia = ""
-    produto_busca = ""
+    # Inicializa como None para não aparecer nada na tela
+    resultados = None
+    
     if request.method == 'POST':
-        if 'pergunta_ia' in request.form:
-            resposta_ia = "Aqui está a resposta da sua pergunta à IA!"
-        if 'produto' in request.form:
-            produto_busca = request.form.get('produto')
-    return render_template('index.html', resposta_ia=resposta_ia, produto=produto_busca, 
-                           mercados=LISTA_MERCADOS, lojas=LISTA_LOJAS, carros=LISTA_CARROS, relogios=LISTA_RELOGIOS)
+        # Se buscar produto, mostra as categorias
+        if 'produto' in request.form and request.form.get('produto'):
+            resultados = {
+                'mercados': LISTA_MERCADOS,
+                'lojas': LISTA_LOJAS,
+                'carros': LISTA_CARROS,
+                'relogios': LISTA_RELOGIOS
+            }
+        # Se perguntar à IA, você processaria aqui (simulado)
+        elif 'pergunta_ia' in request.form:
+            resultados = {'ia_resposta': "Resultado da sua pergunta processado pela IA Optimo."}
+
+    return render_template('index.html', res=resultados)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
