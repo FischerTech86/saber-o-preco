e4b9ef207f33468ff82d0a32370e54379a710c59
FischerTech86 <peixinho1533@gmail.com>
2026-06-22 11:53:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import os
 
 app = Flask(__name__)
 
+# Redireciona a página principal para o seu design
+@app.route('/')
+def home():
+    return '<meta http-equiv="refresh" content="0; url=/resultado">'
+
 @app.route('/resultado')
 def resultado():
-    # Análise da IA
+    # O conteúdo da análise da IA
     analise_ia = "O S23 Ultra é superior em tela e bateria, enquanto o iPhone 15 Pro vence na integração de software."
     return render_template('resultado.html', analise=analise_ia)
 
