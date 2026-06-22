af43d9ad73d34a539be6bbf44c165230215ccc04
FischerTech86 <peixinho1533@gmail.com>
2026-06-22 11:46:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import os
 
 app = Flask(__name__)
 
+# Esta rota redireciona o usuário direto para a comparação
+@app.route('/')
+def home():
+    return '<meta http-equiv="refresh" content="0; url=/resultado">'
+
 @app.route('/resultado')
 def resultado():
-    # Esta é a análise da IA que vai aparecer no seu site
-    texto_da_ia = "O S23 Ultra é superior em produtividade e bateria, enquanto o iPhone 15 Pro vence na integração de software."
-    return render_template('resultado.html', analise=texto_da_ia)
+    # Análise da IA
+    analise_ia = "O S23 Ultra é superior em tela e bateria, enquanto o iPhone 15 Pro vence na integração de software."
+    
+    # O Flask buscará resultado.html dentro da pasta templates/
+    return render_template('resultado.html', analise=analise_ia)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
