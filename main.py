ed5fe27c8d971af4667889b88d491351bb3de2f1
FischerTech86 <peixinho1533@gmail.com>
2026-06-25 08:32:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/resultado')
-def resultado():
-    termo = request.args.get('pesquisa', 'Produto')
+    termo = request.args.get('pesquisa')
+    lista = []
     
-    # Como removemos a IA, vamos criar uma lista de "exemplo" 
-    # para o site ter algo para mostrar.
-    # Você pode alterar esses dados aqui abaixo:
-    lista_de_produtos = [
-        {'nome': f'{termo} Marca A', 'preco': 'R$ 10,00', 'dif': 'Melhor preço', 'link': f'https://www.google.com/search?q={termo.replace(" ", "+")}+marca+a&tbm=shop'},
-        {'nome': f'{termo} Marca B', 'preco': 'R$ 15,00', 'dif': 'Melhor qualidade', 'link': f'https://www.google.com/search?q={termo.replace(" ", "+")}+marca+b&tbm=shop'},
-    ]
+    # Se o usuário pesquisou algo, geramos a lista (simulando dados)
+    if termo:
+        lista = [
+            {'nome': f'{termo} Marca A', 'preco': 'R$ 10,00', 'dif': 'Melhor preço', 'link': f'https://www.google.com/search?q={termo.replace(" ", "+")}+marca+a&tbm=shop'},
+            {'nome': f'{termo} Marca B', 'preco': 'R$ 15,00', 'dif': 'Melhor qualidade', 'link': f'https://www.google.com/search?q={termo.replace(" ", "+")}+marca+b&tbm=shop'},
+        ]
     
-    return render_template('resultado.html', termo=termo, lista=lista_de_produtos)
+    return render_template('index.html', termo=termo, lista=lista)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
