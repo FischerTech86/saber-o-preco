b5f435c8e2059ddba955db7c6d5e88e4dc7da814
FischerTech86 <peixinho1533@gmail.com>
2026-06-27 12:33:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    termo = request.args.get('pesquisa', '')
+    lista = []
+    
+    # Se o usuário digitou algo, geramos os resultados
+    if termo:
+        busca_url = termo.replace(" ", "+")
+        lista = [
+            {'nome': f'{termo} Premium', 'dif': 'Alta qualidade e durabilidade', 'link': f'https://www.google.com/search?q={busca_url}+premium&tbm=shop'},
+            {'nome': f'{termo} Padrão', 'dif': 'Equilíbrio ideal entre preço e qualidade', 'link': f'https://www.google.com/search?q={busca_url}+padrao&tbm=shop'},
+            {'nome': f'{termo} Econômico', 'dif': 'Opção mais barata disponível', 'link': f'https://www.google.com/search?q={busca_url}+barato&tbm=shop'}
+        ]
+        
+    return render_template('index.html', lista=lista)
 
 @app.route('/dicas')
-def dicas():
-    return render_template('dicas.html')
+def dicas(): return render_template('dicas.html')
 
 @app.route('/politica')
-def politica():
-    return render_template('politica.html')
+def politica(): return render_template('politica.html')
 
 @app.route('/sobre')
-def sobre():
-    return render_template('sobre.html')
+def sobre(): return render_template('sobre.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
