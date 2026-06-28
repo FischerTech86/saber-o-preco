bdbde7ee4db25d6c035cd1083c42ca4f964f3649
FischerTech86 <peixinho1533@gmail.com>
2026-06-27 22:00:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    termo = ""
+    if request.method == 'POST':
+        termo = request.form.get('query', '').strip()
+    
+    return render_template('index.html', termo=termo)
 
 if __name__ == '__main__':
     app.run(debug=True)
