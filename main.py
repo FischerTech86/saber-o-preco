5924d043ce79f65f0f2a2eca25cdeab2e12107f1
FischerTech86 <peixinho1533@gmail.com>
2026-06-27 21:50:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    link_google = None
-    produto_nome = ""
-
-    if request.method == 'POST':
-        produto_nome = request.form.get('produto', '').strip()
-        if produto_nome:
-            # Formata o texto para o link do Google (substitui espaços por +)
-            busca_formatada = produto_nome.replace(" ", "+")
-            link_google = f"https://www.google.com/search?tbm=shop&q={busca_formatada}"
-
-    return render_template('index.html', link=link_google, produto=produto_nome)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
