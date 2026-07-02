bd6dacaaf663c7bcecc2f1b0f4369bab843311d3
FischerTech86 <peixinho1533@gmail.com>
2026-07-02 12:38:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Dados para o sistema
-info = {
-    "dicas": ["Verifique o frete antes de comprar.", "Olhe a avaliação da loja."],
-    "politica": "O Optimo facilita sua busca de forma rápida e segura."
-}
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     termo = ""
     if request.method == 'POST':
         termo = request.form.get('query', '').strip()
-    return render_template('index.html', termo=termo, info=info)
+    return render_template('index.html', termo=termo)
 
 if __name__ == '__main__':
     app.run(debug=True)
